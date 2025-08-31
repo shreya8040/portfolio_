129258a3d65da4860c2f7d75d41dc11e63d7a09e
shreya <shreya8040@gmail.com>
2025-08-31 14:50:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
 
+import './Galaxy.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="wrapper">
+        
+        <div className="one">Coding
+          <div className='languages'>
+           <ul><br/>
+            <h4>C, C++</h4>
+            <h4>Java</h4>
+            <h4>Python</h4>
+            <h4>HTML+CSS</h4>
+            <h4>JavaScript</h4>
+            <h4>-React, Node, p5, Electron</h4>
+            <h4>Vibe coding</h4>
+            </ul>
+          </div>
+        </div>
+        <div className="two">About<br/>
+          <div className='about'>I'm Shreya and while I have a ton of interests and hobbies, my twin passions are art and tech.
+                As for art, illustration and various types of design fall under my skillset which I've honed over time.
+                And as for tech, I'm currently pursuing engineering - the core of all tech. 
+                While I enjoy and am continuously learning & coding in different domains ,
+                I also love to learn about the latest tech events, devices and feature drops,
+                how the landscape of tech is evolving, how the world is being impacted by tech in various ways and how I can contribute to this ever evolving field.</div>
+        </div>
+        <div className="three">Illustration and Graphic Design
+            <div className='languages'>
+           <ul><br/>
+            <h4>Krita</h4>
+            <h4>Adobe Fresco</h4>
+            <h4>Infinite painter</h4>
+            
+            </ul>
+          </div>
+          
+        </div>
+        <div className="four">Projects
+          <div className='icon'>
+            <div className='pdes'><a href='https://www.linkedin.com/feed/update/urn:li:activity:7362475425710690304/' target='_blank' rel="noreferrer">PSA Bengaluru</a></div>
+            
+          </div>
+          <div className='desc'>Transit alert app</div>
+          <div className='icon'>
+            <div className='pdes'><a href='https://vibecompass.wixstudio.com/newform'>The Vibe Compass</a></div>
+          </div>
+          <div className='desc'>Entry for Newform web challenge</div>
+        </div>
+        <div className="five">
+          <div className='img'></div>
+        </div>
+        <div className="six">Socials
+          <div className='languages'>
+        <ul>
+           <h4><a href='https://www.linkedin.com/in/shreya-r-0399ab2a1/' target='_blank' rel="noreferrer" className='link'>LinkedIn </a></h4>
+           <h4><a href='https://github.com/shreya8040' target='_blank' rel="noreferrer" className='link'>Github</a></h4>
+           <h4><a href='https://x.com/shreya_r20' target='_blank' rel="noreferrer" className='link'>Twitter
+           <h4><text className='smallt'>(yes it's still twitter)</text></h4></a></h4>
+            
+        </ul>
+          </div>
+        </div>
+        <div className="seven">Certifications
+          <div className='languages'><br/>
+        <ul>
+            <h4><a href ='https://www.linkedin.com/in/shreya-r-0399ab2a1/overlay/1751461749164/single-media-viewer/?profileId=ACoAAEjwsG0Bf9Qdr1WFHGEIMaMT-VP1UHxJmgo'>Bug Bounty contest</a></h4>
+            <h4><a href ='https://drive.google.com/file/d/1yPVjlXXlbazj1U3yXfjRIPpCNAHLs1LT/view'>Deep Dive into Python Libraries</a></h4>
+        </ul>
+        </div>
+        </div>
+        <div className="eight">Favorite Quote
+          <div className='quote'>❝Any sufficiently advanced technology is indistinguishable from magic✨❞</div>
+        </div>
+        <div className='text'>Jack of all trades, skilled at some</div>
+        
+      </div>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Galaxy from './Galaxy';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    
+      <Galaxy>
+        <div className='body1'>
+        <App />
+        </div>
+      
+       </Galaxy>
+    
+    
   </React.StrictMode>
 );
 
